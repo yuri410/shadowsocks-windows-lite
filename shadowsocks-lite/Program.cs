24b14a619003833eee52a82545bd813dc48e3225
yuri410 <canbitwell@gmail.com>
2015-10-29 13:11:51 +0800

    fix path issue

--- shadowsocks-lite/Program.cs
+++ shadowsocks-lite/Program.cs
@@ -3,16 +3,65 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 
 namespace Shadowsocks
 {
     static class Program
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [PreserveSig]
+        public static extern uint GetModuleFileName
+        (
+            [In]
+            IntPtr hModule,
+
+            [Out]
+            StringBuilder lpFilename,
+
+            [In]
+            [MarshalAs(UnmanagedType.U4)]
+            int nSize
+        );
+
+        [DllImport("kernel32.dll")]
+        static extern uint GetLastError();
+
+        const uint ERROR_INSUFFICIENT_BUFFER = 122;
+
+        public static string GetModuleFileName()
+        {
+            StringBuilder exeFilePath = new StringBuilder(8);
+            while (GetModuleFileName(IntPtr.Zero, exeFilePath, exeFilePath.Capacity) != 0)
+            {
+                uint errCode = GetLastError();
+                if (errCode == ERROR_INSUFFICIENT_BUFFER)
+                {
+                    exeFilePath.Clear();
+                    exeFilePath.Capacity = exeFilePath.Capacity * 2;
+                }
+                else if (errCode == 0)
+                    break;
+                else return "";
+            }
+            return exeFilePath.ToString();
+        }
+
+
+
         [STAThread]
         static void Main()
         {
             Util.Utils.ReleaseMemory(true);
+
+            string exePath = GetModuleFileName();
+            if (exePath.Length > 0)
+            {
+                exePath = Path.GetDirectoryName(exePath);
+                Environment.CurrentDirectory = exePath;
+            }
 
             ShadowsocksController controller = new ShadowsocksController();
 
